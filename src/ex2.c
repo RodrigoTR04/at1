9c6e9036b93370217bb18696ca80e8d023d7e8a7
enzolsmoura <enderenzo@gmail.com>
2022-03-18 16:35:10 -0300

    ex1 e ex2 resolvidos

--- src/ex2.c
+++ src/ex2.c
@@ -31,19 +31,81 @@ void exibe_matriz(int matriz[TAM_L][TAM_C]);
 
 int main (int argc, char *argv[])
 {
-
+  int matriz[TAM_L][TAM_C],chave,i,a,l;
+  le_matriz(matriz);
+  printf("Valor a ser buscado: ");
+  scanf("%i", &chave);
+  exibe_matriz(matriz);
+  for(i=0;i<TAM_L;i++){
+    a = verifica_ordenacao(matriz[i]);
+    if(a==0){
+      printf("Linha %i desordenada. Verificando por busca linear.\n",i);
+      l = busca_linear(matriz[i],chave);
+      if(l!=-1){
+        printf("Chave %i encontrada em [%i, %i].",chave,i,l);
+        return 0;
+      }      
+    }
+    if(a==1){
+      printf("Linha %i ordenada. Verificando por busca binária.\n",i);
+      l = busca_binaria(matriz[i],chave);
+      if(l!=-1){
+        printf("Chave %i encontrada em [%i, %i].",chave,i,l);
+        return 0;
+      }
+    }
+  }
+  if(l==-1){
+    printf("Chave %i não foi encontrada.",chave);
+    
+  }
   return 0;
 }
 
-int busca_binaria(int vetor[TAM_C], int chave) {
+int busca_binaria(int vetor[TAM_C], int chave){
+  int i,pos_ini,pos_fim,k;
+  pos_ini = 0;
+  pos_fim = TAM_C-1;
+  while(pos_ini<pos_fim){
+    i = pos_ini + (pos_fim-pos_ini)/2;
+    if(vetor[i]==chave){
+      return i;
+    }
+    if(vetor[i]<chave){
+      pos_ini = i+1;
+    }
+    if(vetor[i]>chave){
+      pos_fim = i-1;
+    }
+  }
+  return -1;
+
 }
 
 int verifica_ordenacao(int vetor[TAM_C]) {
-
-  return -1;
+  int i,ordem;
+  for(i=0;i<TAM_C;i++){
+    if(vetor[i]<vetor[i+1]){
+      ordem = 1;
+    }
+    else{
+      return 0;
+    }
+  }
+  if(ordem==1){
+    return 1;
+  }
+  return 0;
 }
 
 int busca_linear(int vetor[TAM_C], int chave){
+  int i;
+  for(i=0;i<TAM_C;i++){
+    if(chave==vetor[i]){
+      return i;
+    }
+  }
+  return -1;
 }
 
 void le_matriz(int matriz[TAM_L][TAM_C]){
