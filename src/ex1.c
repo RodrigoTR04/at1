b69fa28632fe25d0821c629f6847e77a63453d29
enzolsmoura <enderenzo@gmail.com>
2022-03-18 14:43:53 -0300

    ex1 resolvido

--- src/ex1.c
+++ src/ex1.c
@@ -22,14 +22,51 @@ void exibe_vetor(int vetor[MAX_ARR], int tam_vetor);
 
 int main (int argc, char *argv[])
 {
-
+  int vetor[MAX_ARR],chave,a,b;
+  a = le_vetor(vetor);
+  printf("Chave a ser buscada: ");
+  scanf("%i",&chave);
+  b = busca_linear(vetor,a,chave);
+  exibe_vetor(vetor,a);
+  if(b==-1){
+    printf("Chave %i não foi encontrada.",chave);
+    return 0;
+  }
+  printf("Chave %i encontrada na posição %i.",chave,b);
   return 0;
 }
 
 int busca_linear(int vetor[MAX_ARR], int tam_vetor, int chave) {
+  int i;
+  for(i=0;i<tam_vetor;i++){
+    if(chave==vetor[i]){
+      return i;
+    }
+  }
+  return -1;
 }
 
 int le_vetor(int vetor[MAX_ARR]) {
+  int i, a;
+  printf("Digite o vetor: ");
+  for(i=0;i<MAX_ARR;i++){
+    scanf("%i",&a);
+    if(a==-100){
+      break;
+    } 
+    vetor[i] = a;
+  }
+  /* do
+  {
+    scanf("%i", &a);
+    if (a == -100)
+    {
+      break;
+    }
+    vetor[i] = a;
+    i += 1;
+  } while (i < MAX_ARR); */
+  return i;
 }
 
 void exibe_vetor(int vetor[MAX_ARR], int tam_vetor) {
